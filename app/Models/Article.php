ecb28b638edacac11e6c253037c8ecc8d3da7667
Beatriz Gomes <biaakiddo@gmail.com>
2023-02-18 18:29:48 -0300

    added createFromExternalArticle function

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -26,4 +26,37 @@ class Article extends Model
             'event_id'
         );
     }
+
+    public function createFromExternalArticle($external_article) {
+        $article = Article::create([
+            'external_id' => $external_article->id,
+            'title' => $external_article->title,
+            'url' => $external_article->url,
+            'imageUrl' => $external_article->imageUrl,
+            'newsSite' => $external_article->newsSite,
+            'summary' => $external_article->summary,
+            'publishedAt' => $external_article->publishedAt,
+            'updatedAt' => $external_article->updatedAt,
+            'featured' => $external_article->featured
+        ]);
+
+        foreach($external_article->launches as $launch){
+            $launch = Launch::firstOrCreate([
+                'id' => $external_article->id,
+                'provider' => $external_article->provider
+            ]);
+
+            $article->launches()->attach($launch->id);
+        }
+
+        foreach($external_article->events as $event){
+            $event = Event::firstOrCreate([
+                'id' => $external_article->id,
+                'provider' => $external_article->provider
+            ]);
+
+            $article->events()->attach($event->id);
+        }
+
+    }
 }
