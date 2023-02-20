1da4163732e628ee2a4d9e3ba0808749671491bd
Beatriz Gomes <biaakiddo@gmail.com>
2023-02-20 16:20:46 -0300

    added greet route

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return "Back-end Challenge 2021 🏅 - Space Flight News";
+});
+
 Route::resource('articles', ArticleController::class);
