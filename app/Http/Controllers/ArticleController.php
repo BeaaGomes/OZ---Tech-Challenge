46877ca63cc3b73bff603248d36b0150645cf116
Beatriz Gomes <biaakiddo@gmail.com>
2023-02-21 18:36:30 -0300

    added request params validation

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,13 +6,23 @@ use App\Models\Article;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
     public const PAGE_SIZE = 10;
 
     public function index(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'page' => 'required|integer|min:0',
+        ]);
+
+        if($validator->fails()){
+            return response($validator->errors(), 400);
+        }
+
         $skip_entries = $request->page * self::PAGE_SIZE;
+
         return Article::orderBy('id')
             ->skip($skip_entries)
             ->take(self::PAGE_SIZE)
@@ -22,20 +32,24 @@ class ArticleController extends Controller
     }
 
     public function store(Request $request) {
-        $raw_article = $request->only([
-            'title',
-            'url',
-            'imageUrl',
-            'newsSite',
-            'summary',
-            'publishedAt',
-            'updatedAt',
-            'featured',
-            'launches',
-            'events'
+        $validator = Validator::make($request->all(), [
+            'title' => 'string|required',
+            'url' => 'string|required',
+            'imageUrl' => 'string|present',
+            'newsSite' => 'string|required',
+            'summary' => 'string|present',
+            'publishedAt' => 'date|required',
+            'updatedAt' => 'date|required',
+            'featured' => 'boolean|required',
+            'launches' => 'array|present',
+            'events' => 'array|present',
         ]);
 
-        $new_article = Article::buildArticle($raw_article);
+        if($validator->fails()){
+            return response($validator->errors(), 400);
+        }
+
+        $new_article = Article::buildArticle($validator->valid());
 
         return $this->getArticleWithLaunchesAndEvents($new_article);
     }
@@ -45,38 +59,49 @@ class ArticleController extends Controller
     }
 
     public function update(Request $request, Article $article) {
-        $fields_to_update = $request->only([
-            'title',
-            'url',
-            'imageUrl',
-            'newsSite',
-            'summary',
-            'publishedAt',
-            'updatedAt',
-            'featured'
+        $validator = Validator::make($request->all(), [
+            'title' => 'string',
+            'url' => 'string',
+            'imageUrl' => 'string',
+            'newsSite' => 'string',
+            'summary' => 'string',
+            'publishedAt' => 'date',
+            'updatedAt' => 'date',
+            'featured' => 'boolean',
+            'launches' => 'array',
+            'events' => 'array',
         ]);
+
+        if($validator->fails()){
+            return response($validator->errors(), 400);
+        }
+
+        $fields_to_update = $validator->valid();
 
         if($fields_to_update['publishedAt']){
             $fields_to_update['publishedAt'] = Carbon::parse($fields_to_update['publishedAt']);
         }
-
         if($fields_to_update['updatedAt']){
             $fields_to_update['updatedAt'] = Carbon::parse($fields_to_update['updatedAt']);
         }
 
         $article->update($fields_to_update);
 
-        if(isset($request->launches) && count($request->launches) > 0){
+        $this->updateLaunchesAndEvents($article, $request->launches, $request->events);
+
+        return $this->getArticleWithLaunchesAndEvents($article);
+    }
+
+    private function updateLaunchesAndEvents($article, $launches, $events){
+        if(isset($launches) && count($launches) > 0){
             DB::table('articles_launches')->where('article_id', $article->id)->delete();
         }
 
-        if(isset($request->events) && count($request->events) > 0){
+        if(isset($events) && count($events) > 0){
             DB::table('articles_events')->where('article_id', $article->id)->delete();
         }
 
-        $article->associateLaunchesAndEvents($request->launches, $request->events);
-
-        return $this->getArticleWithLaunchesAndEvents($article);
+        $article->associateLaunchesAndEvents($launches, $events);
     }
 
     private function getArticleWithLaunchesAndEvents($article){
@@ -88,7 +113,5 @@ class ArticleController extends Controller
 
     public function destroy(Article $article) {
         $article->delete();
-
-        return "Article deleted!";
     }
 }
